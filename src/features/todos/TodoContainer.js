fd228823e038ec4a3fad6174e57245972912758c
kaye garcia <kayemgarcia@gmail.com>
2022-01-27 10:04:50 -0600

    Complete

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function TodoContainer() {
+  const todos = useSelector((state) => state.todos.entities);
+
+  console.log(todos);
   return <div>TodoContainer</div>;
 }
 
